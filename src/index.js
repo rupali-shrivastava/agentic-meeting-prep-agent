a2943a1c96eb94df4c9444e5b0bc1df3ec01ff1d
KAPIL RANODE <kranode@BreakthroughT1D.org>
2026-05-26 15:04:52 +0530

    Basic front end work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import meetingData from "./data/mockData.json" assert { type: "json" };
 import { runMeetingAgent } from "./agent/meetingAgent.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/", async (req, res) => {
   const result = await runMeetingAgent(meetingData);
   res.json(result);
+});
+
+app.get("/meetings", (req, res) => {
+  res.json(meetingData);
 });
 
 app.listen(3000, () => {
